5c923f4fca9b2878956fcb385dd4d64ff1981bfa
Ryzen <ryzengreen7@gmail.com>
2022-11-11 22:07:43 +0100

    Finished Load Files

--- lua/autorun/gware-utils-init.lua
+++ lua/autorun/gware-utils-init.lua
@@ -3,11 +3,26 @@ gWare.Utils = gWare.Utils or {}
 
 -- TODO: load files
 if SERVER then
-
+    AddCSLuaFile("gware-utils/commands/sh_akt.lua")
+    AddCSLuaFile("gware-utils/commands/sh_decode.lua")
+    AddCSLuaFile("gware-utils/commands/sh_funk.lua")
+    AddCSLuaFile("gware-utils/commands/sh_looc.lua")
+    AddCSLuaFile("gware-utils/commands/sh_ooc.lua")
+    AddCSLuaFile("gware-utils/commands/sh_roll.lua")
+    AddCSLuaFile("gware-utils/commands/sh_vfunk.lua")
+    AddCSLuaFile("gware-utils/settings/sv_settings.lua")
 end
 
 if CLIENT then
-
+    -- Sowy Leute heute kein GTA
 end
 
--- shared
+-- Shared
+include("gware-utils/commands/sh_akt.lua")
+include("gware-utils/commands/sh_decode.lua")
+include("gware-utils/commands/sh_funk.lua")
+include("gware-utils/commands/sh_looc.lua")
+include("gware-utils/commands/sh_ooc.lua")
+include("gware-utils/commands/sh_roll.lua")
+include("gware-utils/commands/sh_vfunk.lua")
+include("gware-utils/settings/sv_settings.lua")
