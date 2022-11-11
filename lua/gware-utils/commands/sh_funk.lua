9c1e24ad6922b30157c3f73f8f04926aa3de5e60
MenschlichTv <Menschlich@Humanity-media.at>
2022-11-11 23:33:54 +0100

    Started with funk command

--- lua/gware-utils/commands/sh_funk.lua
+++ lua/gware-utils/commands/sh_funk.lua
@@ -8,3 +8,23 @@
     Example Chat: [Funk] *1835 Menschlich an 501st CMD 5125 Ryzen* 10-20u
 ]]
 
+
+if SERVER then
+    hook.Add("PlayerSay", "gWare.Commands.funk", function(ply, chatInput)
+        local text = chatInput:lower()
+
+        if not (text:StartWith("/funk") or text:StartWith("!funk")) then return end
+
+        local args = chatInput:Split("*")
+        local start = message[1]
+,
+        local namePart = start:Replace("!funk", ""):Replace("/funk")
+        local message = args[2]
+
+        local targetedPlayer = gWare.Utils.GetPlayerByNamePart(namePart)
+        local fullName = targetedPlayer and targetedPlayer:Name() or namePart
+
+        net.Start("gWare.Commands.Funk.ChatMessage")
+            net.Write
+    end)
+end
