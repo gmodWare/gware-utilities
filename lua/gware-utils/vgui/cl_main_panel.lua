e8dd366eaa2015e624a165134a8faf9115c04dbc
MenschlichTv <Menschlich@Humanity-media.at>
2022-11-13 23:52:49 +0100

    Added settings UI for bools

--- lua/gware-utils/vgui/cl_main_panel.lua
+++ lua/gware-utils/vgui/cl_main_panel.lua
@@ -9,10 +9,7 @@ function PANEL:Init()
     self.Sidebar = self:Add("VoidUI.Sidebar")
 
     self.Settings = self:Add("gWare.Utils.Settings")
-    self.Sidebar:AddTab("Einstellungen", VoidUI.Icons.Settings, self.Settings, false)
-    self.Sidebar:AddTab("Einstellungen", VoidUI.Icons.Upgrades, self.Settings, false)
-    self.Sidebar:AddTab("Einstellungen", VoidUI.Icons.Trophy, self.Settings, false)
-    self.Sidebar:AddTab("Einstellungen", VoidUI.Icons.Faction, self.Settings, false)
+    self.Sidebar:AddTab("Optionen", VoidUI.Icons.Settings, self.Settings, false)
 end
 
 vgui.Register("gWare.Utils.Frame", PANEL, "VoidUI.Frame")
--- lua/gware-utils/vgui/cl_settings.lua
+++ lua/gware-utils/vgui/cl_settings.lua
@@ -4,7 +4,32 @@ function PANEL:Init()
     self.bg = self:Add("VoidUI.BackgroundPanel")
     self.bg:Dock(FILL)
     self.bg:SDockMargin(10, 10, 10, 10)
-    self.bg:SetTitle("Einstellungen", "")
+
+    local scrollbar = self.bg:Add("VoidUI.ScrollPanel")
+    scrollbar:Dock(FILL)
+    scrollbar:SDockMargin(5, 5, 5, 5)
+
+    for _, data in pairs(gWare.Utils.Settings) do
+        local option = scrollbar:Add("VoidUI.BackgroundPanel")
+        option:Dock(TOP)
+        option:SetTall(60)
+
+        if data.settingType == "bool" then
+            option.input = option:Add("VoidUI.Switch")
+            option.input:SDockMargin(50, 0, 50, 0)
+            option.input:SetChecked(data.value)
+
+            function option.input:OnChange(val)
+                gWare.Utils.UpdateSetting(data.name, val)
+            end
+        end
+
+        option.input:Dock(RIGHT)
+        option.input:MarginRight(10)
+        option.input:SSetWide(90)
+
+        option:SetTitle(data.name, data.description)
+    end
 end
 
 function PANEL:Paint()
