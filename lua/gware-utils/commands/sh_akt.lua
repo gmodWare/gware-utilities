163db18583e907a536f63c147383ef63bb3f95c6
Ryzen <ryzengreen7@gmail.com>
2022-11-13 15:09:19 +0100

    Finished Akt Command

--- lua/gware-utils/commands/sh_akt.lua
+++ lua/gware-utils/commands/sh_akt.lua
@@ -5,3 +5,31 @@
     Example Chat: [Akt] 501st CMD Menschlich schaltet den Reaktor aus.
 ]]
 
+if SERVER then
+    util.AddNetworkString("gWare.Commands.Akt.ChatMessage")
+
+    hook.Add("PlayerSay", "gWare.Commands.Akt", function(ply, text)
+        if (text:lower():StartWith("/akt")) then
+            local message = text:Replace("/akt ", "")
+
+            net.Start("gWare.Commands.Akt.ChatMessage")
+                net.WriteString(message)
+                net.WriteEntity(ply)
+            net.SendToServer()
+        end
+    end)
+end
+
+if CLIENT then
+    local colors = {
+        ["brackets"] = Color(40, 42, 46),
+        ["commandColor"] = Color(75, 92, 245)
+    }
+
+    net.Receive("gWare.Commands.Akt.ChatMessage", function()
+        local receivedMessage = net.ReadString()
+        local ply = net.WriteEntity()
+
+        chat.AddText(colors["brackets"], "[", colors["commandColor"], "AKT", colors["brackets"], "] ", color_white, ply:Nick() .. " " .. receivedMessage)
+    end)
+end
