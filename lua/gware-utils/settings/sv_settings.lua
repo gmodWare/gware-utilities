c9ae901e80324c67dbe47271e8a3145013a0f378
MenschlichTv <Menschlich@Humanity-media.at>
2022-11-14 00:31:34 +0100

    Moved ingame config to sv_settings

--- lua/gware-utils/settings/sv_settings.lua
+++ lua/gware-utils/settings/sv_settings.lua
@@ -39,3 +39,98 @@ function gWare.Utils.ChangeSetting(settingName, settingValue)
     gWare.Utils.UpdateClient(settingName, settingValue)
 end
 
+
+///////////////////////////
+// IN-GAME CONFIGURATION //
+///////////////////////////
+
+gWare.Utils.AddSetting({
+    name = "NPC Waffen Drop",
+    description = "Sollen NPCs beim Tod ihre Waffe fallen lassen?",
+    defaultValue = true,
+    settingType = "bool"
+})
+
+gWare.Utils.AddSetting({
+    name = "Toolgun Geräusche",
+    description = "Soll die Toolgun beim benutzen Geräusche machen?",
+    defaultValue = true,
+    settingType = "bool"
+})
+
+gWare.Utils.AddSetting({
+    name = "Toolgun Effekte",
+    description = "Soll die Toolgun beim benutzen Effekte machen?",
+    defaultValue = true,
+    settingType = "bool"
+})
+
+gWare.Utils.AddSetting({
+    name = "Automatischer Cloak bei Noclip",
+    description = "Soll der Spieler bei Noclip automatisch unsichtbar werden?",
+    defaultValue = false,
+    settingType = "bool"
+})
+
+gWare.Utils.AddSetting({
+    name = "Killfeed anzeigen",
+    description = "Sollen Spielertode rechts oben angezeigt werden?",
+    defaultValue = true,
+    settingType = "bool"
+})
+
+gWare.Utils.AddSetting({
+    name = "Selbstmord erlauben?",
+    description = "Kann ein spieler sich selbst umbringen (Konsolen Befehl)?",
+    defaultValue = true,
+    settingType = "bool"
+})
+
+gWare.Utils.AddSetting({
+    name = "Spieler Waffen drop erlauben?",
+    description = "Kann ein Spieler seine Waffen fallen lassen?",
+    defaultValue = true,
+    settingType = "bool"
+})
+
+gWare.Utils.AddSetting({
+    name = "Automatischer Workshop-Download",
+    description = "Sollen alle User bei joinen die Kollektion automatisch downloaden?",
+    defaultValue = false,
+    settingType = "bool"
+})
+
+gWare.Utils.AddSetting({
+    name = "DarkRP Tafeln aktivieren?",
+    description = "Sollen bei '/advert' Tafeln gespawned werden?",
+    defaultValue = true,
+    settingType = "bool"
+})
+
+gWare.Utils.AddSetting({
+    name = "Family Sharing verbieten?",
+    description = "Sollen User die Family-Sharing benutzen, sofort gekickt werden?",
+    defaultValue = false,
+    settingType = "bool"
+})
+
+gWare.Utils.AddSetting({
+    name = "Sprachanzeige deaktiveren?",
+    description = "Soll die Sprachanzeige Rechts unten deaktiviert werden?",
+    defaultValue = false,
+    settingType = "bool"
+})
+
+gWare.Utils.AddSetting({
+    name = "Spawnmenü für User deaktivieren?",
+    description = "Braucht der user eine permission um das Spawnmenü öffnen zu können?",
+    defaultValue = false,
+    settingType = "bool"
+})
+
+gWare.Utils.AddSetting({
+    name = "Context-Menü für User deaktivieren?",
+    description = "Braucht der user eine permission um das Context-Menü öffnen zu können?",
+    defaultValue = false,
+    settingType = "bool"
+})
