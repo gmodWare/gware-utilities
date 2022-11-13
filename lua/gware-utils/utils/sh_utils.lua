ad003c502c0fd92dbbcb7f71dc415d7919e626e7
MenschlichTv <Menschlich@Humanity-media.at>
2022-11-13 16:31:29 +0100

    Added helper functions with documentation

--- lua/gware-utils/utils/sh_utils.lua
+++ lua/gware-utils/utils/sh_utils.lua
@@ -1,3 +1,7 @@
+--[[
+    Finds a player by a part of its name
+    Returns: player | nil
+]]
 function gWare.Utils.GetPlayerByNamePart(namePart)
     for _, ply in ipairs(player.GetAll()) do
         if ply:Name():lower():find(namePart) then
@@ -6,4 +10,34 @@ function gWare.Utils.GetPlayerByNamePart(namePart)
     end
 
     return nil
+end
+
+--[[
+    Replaces both '/' and '!' including the
+    command name with empty string.
+    Returns: string
+]]
+function string.ReplacePrefix(str, command)
+    local prefix1 = "!" .. command
+    local prefix2 = "/" .. command
+
+    local temp = string.Replace(str, prefix1)
+    
+    return string.Replace(temp, prefix2)
+end
+
+--[[
+    Checks multiple arguments with 'StartWith'
+    Returns: boolean
+]]
+function string.StartWithAny(str, ...)
+    local args = {...}
+
+    for _, v in ipairs(args) do
+        if string.StartWith(str, v) then 
+            return true 
+        end
+    end
+
+    return false
 end
