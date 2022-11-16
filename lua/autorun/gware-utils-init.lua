bda252c7ac6dfefe7abb0c31eff35921909a92a6
Ryzen <ryzengreen7@gmail.com>
2022-11-16 19:24:00 +0100

    Formatet File

--- lua/autorun/gware-utils-init.lua
+++ lua/autorun/gware-utils-init.lua
@@ -7,34 +7,34 @@ local function AddFile(File, dir)
     local fileSide = string.lower(string.Left(File, 3))
 
     if SERVER and fileSide == "sv_" then
-        include(dir..File)
+        include(dir .. File)
     elseif fileSide == "sh_" then
         if SERVER then 
-            AddCSLuaFile(dir..File)
+            AddCSLuaFile(dir .. File)
         end
 
-        include(dir..File)
+        include(dir .. File)
     elseif fileSide == "cl_" then
-        if SERVER then 
-            AddCSLuaFile(dir..File)
+        if SERVER then
+            AddCSLuaFile(dir .. File)
         elseif CLIENT then
-            include(dir..File)
+            include(dir .. File)
         end
     end
 end
 
 local function IncludeDir(dir)
     dir = dir .. "/"
-    local File, Directory = file.Find(dir.."*", "LUA")
+    local File, Directory = file.Find(dir .. "*", "LUA")
 
     for k, v in ipairs(File) do
         if string.EndsWith(v, ".lua") then
             AddFile(v, dir)
         end
     end
-    
+
     for k, v in ipairs(Directory) do
-        IncludeDir(dir..v)
+        IncludeDir(dir .. v)
     end
 end
 
