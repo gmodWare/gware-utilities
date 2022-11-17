2d5efd14da1860b506b3b2e72d8dff1cbf97ffd1
Kreannix <1dufotze@gmail.com>
2022-11-17 17:37:03 +0100

    added setting : ContextMenu, SpawnMenu

--- lua/gware-utils/settings/cl_settings.lua
+++ lua/gware-utils/settings/cl_settings.lua
@@ -6,9 +6,21 @@ hook.Add("gWare.Utils.ClientReady", "gWare.Utils.WaitingForClient", function()
     // Add hooks or functions in here, if u have to wait for client to get the settingsTable
 
     hook.Add("DrawDeathNotice", "gWare.Utils.KillFeed", function()
-        local index = 5
-        if gWare.Utils.GetSettingValue(index) then return end
+        if gWare.Utils.GetSettingValue("showKillfeed") then return end
 
         return false
     end)
+
+    hook.Add("ContextMenuOpen", "C-Menu", function()
+        if not gWare.Utils.GetSettingValue("disableContextmenu") then return end // TODO : add cami permission
+
+        return false
+    end)
+
+    hook.Add( "SpawnMenuOpen", "DisallowSpawnMenu", function()
+        if not gWare.Utils.GetSettingValue("disableSpawnmenu") then return end // TODO : add cami permission
+
+        return false
+    end)
+
 end)
