e47d35b9b96384e545414a5e7bc61eefc29582eb
MenschlichTv <Menschlich@Humanity-Media.at>
2022-12-10 00:37:53 +0100

    refactored permissions

--- lua/gware-utils/permissions/sh_permissions.lua
+++ lua/gware-utils/permissions/sh_permissions.lua
@@ -1,66 +1,52 @@
-CAMI.RegisterPrivilege({
-    Name = "can_access_c-menu",
-    MinAccess = "superadmin",
-    Description = "Can the player open the c-menu?",
-})
+function gWare.Utils.AddPermission(type, name, description)
+    if type == "CAMI" then
+        CAMI.RegisterPrivilege({
+            Name = "can_access_c-menu",
+            MinAccess = "superadmin",
+            Description = "Can the player open the c-menu?",
+        })
+    elseif type == "SAM" then
+        CAMI.UnregisterPrivilege(name)
+        sam.permissions.add(name, "gWare", "superadmin")
+    end
+end
 
-CAMI.RegisterPrivilege({
-    Name = "can_access_spawnmenu",
-    MinAccess = "superadmin",
-    Description = "Can the player open the spawnmenu?",
-})
 
-CAMI.RegisterPrivilege({
-    Name = "can_access_vote",
-    MinAccess = "superadmin",
-    Description = "Can the player open the vote menu?",
-})
+////////////////
+// META TABLE //
+////////////////
 
-CAMI.RegisterPrivilege({
-    Name = "can_change_gware_settings",
-    MinAccess = "superadmin",
-    Description = "Can the player change the gWare settings?",
-})
+local meta = FindMetaTable("Player")
 
-CAMI.RegisterPrivilege({
-    Name = "can_set_jobs-spawns_gware_tool",
-    MinAccess = "superadmin",
-    Description = "Can the player set the job spawns per gWare tool?",
-})
+function meta:HasGWarePermission(name)
+    if SAM_LOADED then
+        return self:HasPermission(name)
+    else
+        return CAMI.PlayerHasAccess(self, name)
+    end
+end
+
+
+gWare.Utils.AddPermission("CAMI", "can_access_c-menu", "Can the player open the c-menu?")
+gWare.Utils.AddPermission("CAMI", "can_access_spawnmenu", "Can the player open the spawnmenu?")
+gWare.Utils.AddPermission("CAMI", "can_access_vote", "Can the player open the vote menu?")
+gWare.Utils.AddPermission("CAMI", "can_change_gware_settings", "Can the player change the gWare settings?")
+gWare.Utils.AddPermission("CAMI", "can_set_jobs-spawns_gware_tool", "Can the player set the job spawns per gWare tool?")
 
 hook.Add("SAM.LoadedConfig", "gWare.Utils.SamModules.WaitForClient", function()
-    CAMI.UnregisterPrivilege("can_access_c-menu")
-    sam.permissions.add("can_access_c-menu", "gWare", "superadmin")
-
-    CAMI.UnregisterPrivilege("can_access_spawnmenu")
-    sam.permissions.add("can_access_spawnmenu", "gWare", "superadmin")
-
-    CAMI.UnregisterPrivilege("can_access_vote")
-    sam.permissions.add("can_access_vote", "gWare", "superadmin")
-
-    CAMI.UnregisterPrivilege("can_change_gware_settings")
-    sam.permissions.add("can_change_gware_settings", "gWare", "superadmin")
-
-    CAMI.UnregisterPrivilege("can_set_jobs-spawns_gware_tool")
-    sam.permissions.add("can_set_jobs-spawns_gware_tool", "gWare", "superadmin")
+    gWare.Utils.AddPermission("SAM", "can_access_c-menu")
+    gWare.Utils.AddPermission("SAM", "can_access_spawnmenu")
+    gWare.Utils.AddPermission("SAM", "can_access_vote")
+    gWare.Utils.AddPermission("SAM", "can_change_gware_settings")
+    gWare.Utils.AddPermission("SAM", "can_set_jobs-spawns_gware_tool")
 end)
 
 hook.Add("OnContextMenuOpen", "gWare.Utils.ContextMenuCheck", function()
     if not gWare.Utils.GetSettingValue("contextmenu") then return end
-
-    if SAM_LOADED then
-        if not LocalPlayer():HasPermission("can_access_c-menu") then return false end
-    else
-        if not CAMI.PlayerHasAccess(LocalPlayer(), "can_access_c-menu") then return false end
-    end
+    if not LocalPlayer():HasGWarePermission("can_access_c-menu") then return false end
 end)
 
 hook.Add("OnSpawnMenuOpen", "gWare.Utils.SpawnMenuCheck", function()
     if not gWare.Utils.GetSettingValue("spawnmenu") then return end
-
-    if SAM_LOADED then
-        if not LocalPlayer():HasPermission("can_access_spawnmenu") then return false end
-    else
-        if not CAMI.PlayerHasAccess(LocalPlayer(), "can_access_spawnmenu") then return false end
-    end
+    if not LocalPlayer():HasGWarePermission("can_access_spawnmenu") then return false end
 end)
