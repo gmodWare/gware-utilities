005ad51fbcc43b65e32128e460d2a24a37059cf7
MenschlichTv <Menschlich@Humanity-Media.at>
2022-12-12 14:19:45 +0100

    updated icon

--- lua/gware-utils/vgui/cl_main_panel.lua
+++ lua/gware-utils/vgui/cl_main_panel.lua
@@ -12,7 +12,7 @@ function PANEL:Init()
     self.Vote = self:Add("gWare.Utils.Vote")
 
     self.Sidebar:AddTab("Optionen", VoidUI.Icons.Settings, self.Settings, false)
-    self.Sidebar:AddTab("Vote", VoidUI.Icons.Settings, self.Vote, false)
+    self.Sidebar:AddTab("Vote", VoidUI.Icons.Stats, self.Vote, false)
 end
 
 vgui.Register("gWare.Utils.Frame", PANEL, "VoidUI.Frame")
