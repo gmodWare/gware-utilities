0d4f3d62b8815b0d86c36aef615da4a4e6f7ac05
kxx <1dufotze@gmail.com>
2022-12-12 18:19:02 +0100

    update : fixed ryzens shit code, submit wont work now

--- lua/gware-utils/vgui/cl_vote.lua
+++ lua/gware-utils/vgui/cl_vote.lua
@@ -1,86 +1,96 @@
 local PANEL = {}
-local scrw, scrh = ScrW(), ScrH()
-
-local function sc(x)
-    return x / 1080 * scrh
-end
 
 function PANEL:Init()
-    local scrollbar = self:Add("VoidUI.ScrollPanel")
-    scrollbar:Dock(FILL)
-    scrollbar:SDockMargin(5, 5, 5, 5)
+    self.backgroundPanel = self:Add("VoidUI.BackgroundPanel")
+    self.backgroundPanel:Dock(FILL)
+    self.backgroundPanel:SDockMargin(10, 10, 10, 10)
 
-    local questionV = ""
+    self.scrollBar = self.backgroundPanel:Add("VoidUI.ScrollPanel")
+    self.scrollBar:Dock(FILL)
 
-    local question = scrollbar:Add("VoidUI.TextInput")
-    question:SSetSize(300, 55)
-    question:Dock(TOP)
-    question:DockMargin(10, 10, 10, 10)
-    question:SetValue("Question:")
-
-    questionV = question:GetValue()
-
-    local answers = 3
-
-    local answer
-
-    local valueTbl = {}
-
-    for i = 1, answers do
-        answer = scrollbar:Add("VoidUI.TextInput")
-        answer:SSetSize(250, 50)
-        answer:Dock(TOP)
-        answer:DockMargin(10, 10, 10, 10)
-        answer:SetValue("Answer " .. i)
-        answer.OnValueChange = function()
-            valueTbl[#valueTbl + 1] = answer:GetValue()
-        end
+    self.questionTextPanel = self.scrollBar:Add("DPanel")
+    self.questionTextPanel:SSetPos(5, 0)
+    self.questionTextPanel:SSetSize(550, 50)
+    self.questionTextPanel.Paint = function(s, w, h)
+        draw.SimpleText("Question:", "VoidUI.R20", 0, 0, VoidUI.Colors.White, TEXT_ALIGN_LEFT, TEXT_ALIGN_LEFT)
     end
 
-    local addNewAnswer = scrollbar:Add("VoidUI.Button")
-    addNewAnswer:SSetSize(250, 50)
-    addNewAnswer:Dock(TOP)
-    addNewAnswer:DockMargin(10, 10, 10, 10)
-    addNewAnswer:SetText("Add New Answer")
-    addNewAnswer.DoClick = function()
-        answers = answers + 1
+    self.choiceTextPanel = self.scrollBar:Add("DPanel")
+    self.choiceTextPanel:SSetPos(5, 100)
+    self.choiceTextPanel:SSetSize(550, 50)
+    self.choiceTextPanel.Paint = function(s, w, h)
+        draw.SimpleText("Multiple Choice:", "VoidUI.R20", 0, 0, VoidUI.Colors.White, TEXT_ALIGN_LEFT, TEXT_ALIGN_LEFT)
     end
 
-    local sendVote = scrollbar:Add("VoidUI.Button")
-    sendVote:SSetSize(250, 50)
-    sendVote:Dock(TOP)
-    sendVote:DockMargin(10, 10, 10, 10)
-    sendVote:SetText("Send Vote")
-    sendVote.DoClick = function()
+    self.question = self.scrollBar:Add("VoidUI.TextInput")
+    self.question:SSetPos(0, 30)
+    self.question:SSetSize(550, 50)
+    self.question:SetPlaceholder("Question . . . ?")
 
-        print("Do Click!")
+    local yPos = 130
+    local choiceCounter = 1
 
-        questionV = question:GetValue()
+    for i = 1, 2 do
+        local choice = self.scrollBar:Add("VoidUI.TextInput")
+        choice:SSetPos(0, yPos)
+        choice:SSetSize(550, 50)
+        choice:SetPlaceholder("Choice " .. i)
 
-        if (questionV == "") or (questionV == nil) or (questionV == "Question:") then
-            VoidLib.Notify("ERROR", "You didn't have give a Question", VoidUI.Colors.Red, 5)
+        choiceCounter = choiceCounter + 1
+        yPos = yPos + 60
+    end
+
+    self.addChoice = self.scrollBar:Add("VoidUI.Button")
+    self.addChoice:SSetPos(0, yPos + 5)
+    self.addChoice:SSetSize(550, 35)
+    self.addChoice:SetText("Add Choice")
+    self.addChoice:SetFont("VoidUI.R20")
+    self.addChoice:SetColor(VoidUI.Colors.Blue)
+    self.addChoice.DoClick = function(s)
+        if choiceCounter >= 5 + 1 then
+            VoidLib.Notify("gWare", "Du kannst du nur 5 Wahlmöglichkeiten haben!", VoidUI.Colors.Red, 5)
             return
         end
 
+        local choice = self.scrollBar:Add("VoidUI.TextInput")
+        choice:SSetPos(0, yPos)
+        choice:SSetSize(550, 50)
+        choice:SetPlaceholder("Choice " .. choiceCounter)
 
-        self:Remove()
+        choiceCounter = choiceCounter + 1
+        yPos = yPos + 60
 
-        net.Start("gWare.Utils.VoteSystem.SendVoteToServer")
-            net.WriteString(questionV)
-            for k, v in ipairs(valueTbl) do
-                net.WriteString(v)
+        self.addChoice:SSetPos(0, yPos + 5)
+        self.submit:SSetPos(0, yPos + 70)
+    end
+
+    local values = {}
+
+    self.submit = self.backgroundPanel:Add("VoidUI.Button")
+    self.submit:Dock(BOTTOM)
+    self.submit:SDockMargin(0, 10, 25, 0)
+    self.submit:SSetSize(550, 50)
+    self.submit:SetFont("VoidUI.R28")
+    self.submit:SetText("Submit")
+    self.submit.DoClick = function()
+        for _, panelData in ipairs(self.scrollBar:GetChildren()[1]:GetChildren()) do
+            if panelData:GetName() != "VoidUI.TextInput" then continue end
+
+            if panelData:GetValue() == "" then
+                VoidLib.Notify("gWare", "Du musst alle Felder ausfüllen!", VoidUI.Colors.Red, 5)
+
+                table.Empty(values)
+                break
             end
-            net.WriteUInt(#valueTbl, 3)
-        net.SendToServer()
 
-        print("Sended Netmessage!")
+            table.insert(values, panelData:GetValue())
+        end
 
-        VoidLib.Notify("SUCCESSFULLY", "You have start a vote", VoidUI.Colors.Green, 5)
+        gWare.Utils.SendVoteToServer(value)
     end
 end
 
-function PANEL:Paint()
-
+function PANEL:Paint(w, h)
 end
 
 vgui.Register("gWare.Utils.Vote", PANEL)
