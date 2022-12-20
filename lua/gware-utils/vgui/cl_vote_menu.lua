b2d3c1b83575f3eeced728d5bda051256f0d7b97
kxx <1dufotze@gmail.com>
2022-12-20 17:20:50 +0100

    update : selected the vote by pressing the key 1-5

--- lua/gware-utils/vgui/cl_vote_menu.lua
+++ lua/gware-utils/vgui/cl_vote_menu.lua
@@ -19,6 +19,22 @@ function PANEL:SendResultToServer()
         net.Start("gWare.Utils.SendResultToServer")
             net.WriteUInt(pick, 3)
         net.SendToServer()
+
+        hook.Remove("PlayerButtonDown", "gWare.Utils.RegisterVoteByKeyInput")
+    end)
+end
+
+function PANEL:RegisterVoteByKeyInput()
+    hook.Add("PlayerButtonDown", "gWare.Utils.RegisterVoteByKeyInput", function(ply, key)
+        if not IsFirstTimePredicted() then return end
+
+        if self.valueTable[key] then
+            local voteButton = self.scrollBar:GetChildren()[1]:GetChildren()
+            local choice = key - 1
+
+            self:SetActivePanel(voteButton[choice])
+            pick = choice
+        end
     end)
 end
 
@@ -31,6 +47,7 @@ function PANEL:Init()
     self:SetKeyboardInputEnabled(false)
 
     self:SendResultToServer()
+    self:RegisterVoteByKeyInput()
 end
 
 function PANEL:Vote()
@@ -56,7 +73,7 @@ function PANEL:Vote()
                 draw.RoundedBox(12, 0, 0, w, h, VoidUI.Colors.Blue)
             end
 
-            draw.SimpleText(index - 1 .. "  |  " .. values, "VoidUI.R20", 5, 5, VoidUI.Colors.White, TEXT_ALIGN_LEFT, TEXT_ALIGN_LEFT)
+            draw.SimpleText(index - 1 .. "  |  " .. values, "VoidUI.R20", 5, 8, VoidUI.Colors.White, TEXT_ALIGN_LEFT, TEXT_ALIGN_LEFT)
         end
 
         button.DoClick = function()
@@ -72,6 +89,10 @@ function PANEL:Paint(w, h)
     self.timeLeft = math.Round(timer.TimeLeft("gWare.Vote"))
 
     self:SetTitle(self.valueTable[1] .. " (" .. self.timeLeft .. ")")
+end
+
+function PANEL:OnRemove()
+    hook.Remove("PlayerButtonDown", "gWare.Utils.RegisterVoteByKeyInput")
 end
 
 vgui.Register("gWare.Utils.VoteMenu", PANEL, "VoidUI.Frame")
