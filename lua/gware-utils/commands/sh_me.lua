f8bc9e2421a4e555e7bcb102c00d0cf715f3e19c
kxx <1dufotze@gmail.com>
2022-12-28 00:30:21 +0100

    refactored me and ooc command

--- lua/gware-utils/commands/sh_me.lua
+++ lua/gware-utils/commands/sh_me.lua
@@ -5,43 +5,34 @@
     Example Chat: [me] 501st CMD Menschlich: Lol der ist einfach umgefallen xD
 ]]
 
-if SERVER then
-    util.AddNetworkString("gWare.Commands.Me.ChatMessage")
+local command = gWare.Utils.RegisterCommand({
+    prefix = "me",
+    triggers = {"me"},
+})
 
-    hook.Add("PlayerSay", "gWare.Commands.me", function(sender, text)
-        if not text:StartWithAny("/me ") then return end
+command:OnServerSide(function(sender, message)
+    if gWare.Utils.IsMessageEmpty(message, sender) then return "" end
 
-        if gWare.Utils.GetSettingValue("command_me") then return end
+    local receivers = {sender}
+    local distSqr = 500 * 500
 
-        local message = text:ReplacePrefix("me")
+    for _, ply in ipairs(player.GetAll()) do
+        if ply:GetPos():DistToSqr(sender:GetPos()) > distSqr then continue end
 
-        if gWare.Utils.IsMessageEmpty(message, sender) then return "" end
+        table.insert(receivers, ply)
+    end
 
-        local receivers = {sender}
-        local distSqr = 500 * 500
+    net.Start(command:GetNetID())
+        net.WriteString(message)
+        net.WriteEntity(sender)
+    net.Send(receivers)
+end)
 
-        for _, ply in ipairs(player.GetAll()) do
-            if ply:GetPos():DistToSqr(sender:GetPos()) > distSqr then continue end
+command:OnReceive(function()
+    local message = net.ReadString()
+    local ply = net.ReadEntity()
 
-            table.insert(receivers, ply)
-        end
+    local color = RPExtraTeams[ply:Team()].color
 
-        net.Start("gWare.Commands.Me.ChatMessage")
-            net.WriteString(message)
-            net.WriteEntity(sender)
-        net.Send(receivers)
-
-        return ""
-    end)
-end
-
-if CLIENT then
-    net.Receive("gWare.Commands.Me.ChatMessage", function()
-        local message = net.ReadString()
-        local sender = net.ReadEntity()
-
-        local color = RPExtraTeams[sender:Team()].color
-
-        chat.AddText(color, sender:Name() .. " " .. message, color_white)
-    end)
-end
+    chat.AddText(color, ply:Name() .. " " .. message, color_white)
+end)
--- lua/gware-utils/commands/sh_ooc.lua
+++ lua/gware-utils/commands/sh_ooc.lua
@@ -6,47 +6,27 @@
     Example Chat: [LOOC] 501st CMD Menschlich: Lol der ist eingach umgefallen xD
 ]]
 
-if SERVER then
-    util.AddNetworkString("gWare.Commands.OOC.ChatMessage")
+local command = gWare.Utils.RegisterCommand({
+    prefix = "ooc",
+    triggers = {"ooc", "/"},
+})
 
-    hook.Add("DarkRPFinishedLoading", "gWare.Commands.OOC", function()
-        DarkRP.removeChatCommand("ooc")
-    end)
+command:OnServerSide(function(sender, message)
+    if gWare.Utils.IsMessageEmpty(message, sender) then return "" end
 
-    hook.Add("PlayerSay", "gWare.Commands.OOC", function(ply, text)
-        local symbol = text[1]
-        if not text:StartWithAny(symbol .. "ooc ", "// ") then return end
+    net.Start(command:GetNetID())
+        net.WriteString(message)
+        net.WriteEntity(sender)
+    net.Broadcast()
+end)
 
-        if gWare.Utils.GetSettingValue("command_ooc") then return end
+command:OnReceive(function()
+    local message = net.ReadString()
+    local sender = net.ReadEntity()
 
-        local message
+    local senderColor = RPExtraTeams[sender:Team()].color
 
-        if text:StartWith(symbol .. "ooc ") then
-            message = text:sub(6)
-        else
-            message = text:sub(4)
-        end
-
-        if gWare.Utils.IsMessageEmpty(message, ply) then return "" end
-
-        net.Start("gWare.Commands.OOC.ChatMessage")
-            net.WriteString(message)
-            net.WriteEntity(ply)
-        net.Broadcast()
-
-        return ""
-    end)
-end
-
-if CLIENT then
-    net.Receive("gWare.Commands.OOC.ChatMessage", function()
-        local message = net.ReadString()
-        local sender = net.ReadEntity()
-
-        local senderColor = RPExtraTeams[sender:Team()].color
-
-        gWare.Utils.PrintCommand("ooc",
-            senderColor, sender:Nick(),  gWare.Utils.Colors.Brackets, " » ", color_white, message
-        )
-    end)
-end
+    gWare.Utils.PrintCommand(command:GetPrefix(),
+        senderColor, sender:Nick(),  gWare.Utils.Colors.Brackets, " » ", color_white, message
+    )
+end)
