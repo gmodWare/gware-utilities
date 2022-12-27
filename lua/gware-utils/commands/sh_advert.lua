cad4ae1118d1011821e585f15b88fd486ba23024
kxx <1dufotze@gmail.com>
2022-12-27 22:40:14 +0100

    refactored advert

--- lua/gware-utils/commands/sh_advert.lua
+++ lua/gware-utils/commands/sh_advert.lua
@@ -5,39 +5,29 @@
     Example Chat: [Advert] Kommt zu Nexus Gaming
 ]]
 
-if SERVER then
-    util.AddNetworkString("gWare.Commands.Advert.ChatMessage")
+local command = gWare.Utils.RegisterCommand({
+    prefix = "advert",
+    triggers = {"advert"},
+})
 
-    hook.Add("PlayerSay", "gWare.Commands.Advert", function(ply, text)
-        if not gWare.Utils.GetSettingValue("billboards") then return end
+command:OnServerSide(function(ply, message)
+    if not gWare.Utils.GetSettingValue("billboards") then return end
 
-        if (text:lower():StartWithAny("/advert ")) then
+    if gWare.Utils.IsMessageEmpty(message, ply) then return "" end
 
-            if gWare.Utils.GetSettingValue("command_advert") then return end
+    net.Start("gWare.Commands.Advert.ChatMessage")
+        net.WriteString(message)
+        net.WriteEntity(ply)
+    net.Broadcast()
+end)
 
-            local message = text:ReplacePrefix("advert")
+command:OnReceive(function()
+    local receivedMessage = net.ReadString()
+    local ply = net.ReadEntity()
 
-            if gWare.Utils.IsMessageEmpty(message, ply) then return "" end
+    local playerColor = RPExtraTeams[ply:Team()].color
 
-            net.Start("gWare.Commands.Advert.ChatMessage")
-                net.WriteString(message)
-                net.WriteEntity(ply)
-            net.Broadcast()
-
-            return ""
-        end
-    end)
-end
-
-if CLIENT then
-    net.Receive("gWare.Commands.Advert.ChatMessage", function()
-        local receivedMessage = net.ReadString()
-        local ply = net.ReadEntity()
-
-        local playerColor = RPExtraTeams[ply:Team()].color
-
-        gWare.Utils.PrintCommand("advert",
-            playerColor, ply:Nick(), gWare.Utils.Colors.Brackets, " » ", color_white, receivedMessage
-        )
-    end)
-end
+    gWare.Utils.PrintCommand("advert",
+        playerColor, ply:Nick(), gWare.Utils.Colors.Brackets, " » ", color_white, receivedMessage
+    )
+end)
