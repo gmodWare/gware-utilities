404241de83a815789e87ffe19090c851a0910c93
MenschlichTv <Menschlich@Humanity-Media.at>
2022-12-27 22:58:03 +0100

    refactored unstuck command

--- lua/gware-utils/commands/sh_unstuck.lua
+++ lua/gware-utils/commands/sh_unstuck.lua
@@ -5,58 +5,55 @@
     Example Chat: [Unstuck] You have 5 seconds to move out of the object.
 ]]
 
+-- this code might be a bit scuffed, because we cannot properly use our command handler :c
+-- -menschlich
+
+
 local L = gWare.Utils.Lang.GetPhrase
 
-if SERVER then
-    util.AddNetworkString("gWare.Commands.unstuck.ChatMessage")
+local command = gWare.Utils.RegisterCommand({
+    prefix = "unstuck",
+    triggers = {"unstuck"}
+})
 
-    local delay = {}
-    local noCollidedPlayers = {}
+local delay = {}
+local noCollidedPlayers = {}
 
-    hook.Add("PlayerSay", "gWare.Commands.unstuck", function(ply, chatInput)
-        local text = chatInput:lower()
-
-        if not text:StartWithAny("!unstuck", "/unstuck") then return end
-
-        if gWare.Utils.GetSettingValue("command_unstuck") then return end
-
-        if (delay[ply] or 0) > CurTime() then
-            VoidLib.Notify(ply, L("notify_unstuck-error_name"), L("notify_unstuck-error-cooldown_desc"), VoidUI.Colors.Red, 2)
-            return ""
-        end
-
-        local traceHull = util.TraceHull({
-            start = ply:GetPos(),
-            endpos = ply:GetPos(),
-            mins = Vector(-16, -16, 0),
-            maxs = Vector(16, 16, 72),
-            filter = ply
-        })
-
-        if (not traceHull.Hit or not IsValid(traceHull.Entity)) then
-            VoidLib.Notify(ply, L("notify_unstuck-error_name"), L("notify_unstuck-error-invalid_desc"), VoidUI.Colors.Red, 2)
-            return ""
-        end
-
-        timer.Simple(3, function()
-            if IsValid(ply) then
-                noCollidedPlayers[ply] = nil
-                ply:SetCustomCollisionCheck(false)
-            end
-        end)
-
-        ply:SetCustomCollisionCheck(true)
-        noCollidedPlayers[ply] = true
-        VoidLib.Notify(ply, L("notify_unstuck_name"), L("notify_unstuck-start_desc"), VoidUI.Colors.Green, 4)
-
-        delay[ply] = CurTime() + 15
-
+command:OnServerSide(function(ply)
+    if (delay[ply] or 0) > CurTime() then
+        VoidLib.Notify(ply, L("notify_unstuck-error_name"), L("notify_unstuck-error-cooldown_desc"), VoidUI.Colors.Red, 2)
         return ""
-    end)
+    end
 
-    hook.Add("ShouldCollide", "gWare.Commands.unstuck", function(ent1, ent2)
-        if (ent1:IsPlayer() and noCollidedPlayers[ent1]) or (ent2:IsPlayer() and noCollidedPlayers[ent2]) then
-            return false
+    local traceHull = util.TraceHull({
+        start = ply:GetPos(),
+        endpos = ply:GetPos(),
+        mins = Vector(-16, -16, 0),
+        maxs = Vector(16, 16, 72),
+        filter = ply
+    })
+
+    if (not traceHull.Hit or not IsValid(traceHull.Entity)) then
+        VoidLib.Notify(ply, L("notify_unstuck-error_name"), L("notify_unstuck-error-invalid_desc"), VoidUI.Colors.Red, 2)
+        return ""
+    end
+
+    timer.Simple(3, function()
+        if IsValid(ply) then
+            noCollidedPlayers[ply] = nil
+            ply:SetCustomCollisionCheck(false)
         end
     end)
-end
+
+    ply:SetCustomCollisionCheck(true)
+    noCollidedPlayers[ply] = true
+    VoidLib.Notify(ply, L("notify_unstuck_name"), L("notify_unstuck-start_desc"), VoidUI.Colors.Green, 4)
+
+    delay[ply] = CurTime() + 15
+end)
+
+hook.Add("ShouldCollide", "gWare.Commands.unstuck", function(ent1, ent2)
+    if (ent1:IsPlayer() and noCollidedPlayers[ent1]) or (ent2:IsPlayer() and noCollidedPlayers[ent2]) then
+        return false
+    end
+end)
